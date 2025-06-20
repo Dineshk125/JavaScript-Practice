4205b0b50353261fc0e09aaa027f9c754fbb1759
Dinesh Kumawat <dineshkumawatdtr123@gmail.com>
2025-06-20 09:54:49 +0530

    infinit loop practice

---
+++ infinite loop.js
@@ -0,0 +1,15 @@
+// for (i = 1; ; i++){
+//     console.log(i)
+// }
+
+// console.log("hii")
+
+// for (i = 1; i<=5 ; i++){
+//     console.log(i)
+// }
+
+// for (i = 1; i>=0 ; i++){
+//     console.log(i)
+// }
+
+
